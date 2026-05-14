a0abc7fa34577551c2fb0b5a9792ed3a2e7905d6
shubh <shubhvjain@gmail.com>
2026-05-14 14:06:05 +0200

    ppi network setup

--- src/tfitpy/__init__.py
+++ src/tfitpy/__init__.py
@@ -1,4 +1,7 @@
 from tfitpy.main import compute_indices,load_cache
 
 from tfitpy.datasets.gene_names import convert_gene_df
-_all_=["load_cache","compute_indices","convert_gene_df"]
+
+from tfitpy.network_analysis_ppi.generate_null import generate_null_networks
+
+_all_=["load_cache","compute_indices","convert_gene_df","generate_null_networks"]
---
+++ src/tfitpy/network_analysis_ppi/__init__.py
@@ -0,0 +1,5 @@
+from tfitpy.network_analysis_ppi.generate_null import generate_null_networks
+
+__all__ = [
+    "generate_null_networks"
+]

75671632051ae1253bead77d55ebf28fe4635ac5
shubh <shubhvjain@gmail.com>
2026-05-14 22:17:08 +0200

    new method to compute socores

--- src/tfitpy/__init__.py
+++ src/tfitpy/__init__.py
@@ -4,4 +4,9 @@ from tfitpy.datasets.gene_names import convert_gene_df
 
 from tfitpy.network_analysis_ppi.generate_null import generate_null_networks
 
-_all_=["load_cache","compute_indices","convert_gene_df","generate_null_networks"]
+from tfitpy.network_analysis_ppi.main import load_all_network_data
+
+from tfitpy.network_analysis_ppi.index import compute_module_network_metrics
+
+
+_all_=["load_cache","compute_indices","convert_gene_df","generate_null_networks","load_all_network_data","compute_module_network_metrics"]
--- src/tfitpy/network_analysis_ppi/__init__.py
+++ src/tfitpy/network_analysis_ppi/__init__.py
@@ -1,5 +1,8 @@
 from tfitpy.network_analysis_ppi.generate_null import generate_null_networks
-
+from tfitpy.network_analysis_ppi.main import load_all_network_data
+from tfitpy.network_analysis_ppi.index import compute_module_network_metrics
 __all__ = [
-    "generate_null_networks"
+    "generate_null_networks",
+    "load_all_network_data",
+    "compute_module_network_metrics"
 ]
